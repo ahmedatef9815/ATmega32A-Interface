791a22eaf553ea0455d4126d06d83d482b42022c
ahmedatef9815 <1700106@eng.asu.edu.eg>
2021-10-08 15:01:24 +0200

    Second Commit

--- StepperMotor_Interface/StepperMotor.c
+++ StepperMotor_Interface/StepperMotor.c
@@ -1,8 +1,15 @@
 #include "StepperMotor.h"
 #include "util/delay.h"
 
+#define STEPPER_MOTOR_COILS_NUM  (4U)
+
 //g for global , a for array , u8 for uint8
-uint8 gau8StepperRotation[4U] = {0x10U, 0x20U, 0x40U, 0x80U};
+#if STEPPER_UPPER_PORT
+uint8 gau8StepperRotation[STEPPER_MOTOR_COILS_NUM] = {0x10U, 0x20U, 0x40U, 0x80U};
+#else
+uint8 gau8StepperRotation[STEPPER_MOTOR_COILS_NUM] = {0x01U, 0x02U, 0x04U, 0x08U};
+#endif
+
 
 void StepperMotorInit(void)
 {
@@ -15,10 +22,14 @@ void StepperMotorInit(void)
 void StepperMotorTurnClockwise(void)
 {
 	uint8 u8LocalLoopIndex;
-	for (u8LocalLoopIndex = 0U; u8LocalLoopIndex < 4U; u8LocalLoopIndex++)
+	for (u8LocalLoopIndex = 0U; u8LocalLoopIndex < STEPPER_MOTOR_COILS_NUM; u8LocalLoopIndex++)
 	{
-		
-		*STEPPER_MOTOR_OUT_REG &= 0x0FU;              //lazem asfar el 4 bits el 3ayz akteb feehom kol mara
+		#if STEPPER_UPPER_PORT
+		*STEPPER_MOTOR_OUT_REG &= 0x0FU;     //lazem asfar el 4 bits el 3ayz akteb feehom kol mara
+		#else
+		*STEPPER_MOTOR_OUT_REG &= 0xF0U;
+		#endif
+		             
 		*STEPPER_MOTOR_OUT_REG |= gau8StepperRotation[u8LocalLoopIndex];
 		_delay_ms(STEPPER_MOTOR_STEP_DELAY_MS);
 	}
@@ -28,10 +39,14 @@ void StepperMotorTurnClockwise(void)
 void StepperMotorTurnCounterClockwise(void)
 {
 	sint8 s8LocalLoopIndex;
-	for (s8LocalLoopIndex = 3; s8LocalLoopIndex >= 0; s8LocalLoopIndex--)
+	for (s8LocalLoopIndex = (STEPPER_MOTOR_COILS_NUM - 1); s8LocalLoopIndex >= 0; s8LocalLoopIndex--)
 	{
-		
-		*STEPPER_MOTOR_OUT_REG &= 0x0FU;              //lazem asfar el 4 bits el 3ayz akteb feehom kol mara
+		#if STEPPER_UPPER_PORT
+		*STEPPER_MOTOR_OUT_REG &= 0x0FU;         //lazem asfar el 4 bits el 3ayz akteb feehom kol mara
+		#else 
+		*STEPPER_MOTOR_OUT_REG &= 0xF0U;
+		#endif
+		              
 		*STEPPER_MOTOR_OUT_REG |= gau8StepperRotation[s8LocalLoopIndex];
 		_delay_ms(STEPPER_MOTOR_STEP_DELAY_MS);
 	}
